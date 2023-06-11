91ff937bbba813d1e22edde1b86b60d6723701b1
Eduardo0409 <100179167+Eduardo0409@users.noreply.github.com>
2023-06-11 12:33:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PAR-ou-IMPAR
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-b5k3jq)
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: `./main.html`,
+})
+export class App {
+  name = 'MyApp';
+  n: number = 10;
+  numbers: number[];
+
+  update(){
+    this.numbers = [];
+    for(let i=1; i<=this.n; i++){
+      let r = Math.trunc(100 * Math.random());
+      this.numbers.push(r);
+    }
+  }
+}
+
+bootstrapApplication(App);
